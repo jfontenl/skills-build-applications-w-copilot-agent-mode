74c056c871f1c1e1120e4e9b0b06347112e28bc1
jfontenl <jfontenl@emeal.nttdata.com>
2025-10-29 13:43:02 +0000

    Step 5, commit3

--- octofit-tracker/backend/octofit_tracker/serializers.py
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -6,22 +6,57 @@ class TeamSerializer(serializers.ModelSerializer):
         model = Team
         fields = '__all__'
 
+
 class UserSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    team = serializers.SerializerMethodField()
+
+    def get_id(self, obj):
+        return str(obj.id)
+
+    def get_team(self, obj):
+        return str(obj.team.id) if obj.team else None
+
     class Meta:
         model = User
         fields = '__all__'
 
 class ActivitySerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
+
+    def get_id(self, obj):
+        return str(obj.id)
+
+    def get_user(self, obj):
+        return str(obj.user.id) if obj.user else None
+
     class Meta:
         model = Activity
         fields = '__all__'
 
+
 class LeaderboardSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
+
+    def get_id(self, obj):
+        return str(obj.id)
+
+    def get_user(self, obj):
+        return str(obj.user.id) if obj.user else None
+
     class Meta:
         model = Leaderboard
         fields = '__all__'
 
+
 class WorkoutSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+
+    def get_id(self, obj):
+        return str(obj.id)
+
     class Meta:
         model = Workout
         fields = '__all__'
